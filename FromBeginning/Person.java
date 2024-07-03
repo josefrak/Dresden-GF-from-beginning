8bc3e0297b69274f896c1f5c7bcde04e4fbfaa9f
Josef Rak <peparak@gmail.com>
2024-07-03 10:09:33 +0200

    Prepare class person

--- FromBeginning/Person.java
+++ FromBeginning/Person.java
@@ -6,12 +6,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Pearson extends Actor
+public class Person extends Actor
 {
-    /**
-     * Act - do whatever the Pearson wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private int delay;
+    private int delayCounter;
+    public Person(){
+        this.delayCounter = 0;
+        this.delay = 3;  
+    }       
     public void act()
     {
         // Add your action code here.
