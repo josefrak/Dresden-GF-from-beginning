4e9e7501788bfcc3d35a7cb6447b11510c4a14c1
Josef Rak <peparak@gmail.com>
2024-07-03 09:41:54 +0200

    Change - random place of the robber + different syntax of calling method with more paramaters

--- FromBeginning/Beach.java
+++ FromBeginning/Beach.java
@@ -18,7 +18,11 @@ public class Beach extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(15, 10, 60); 
         this.addObject(new Police("w","s","a","d"), 0, 0);
-        this.addObject(new Robber(), 15, 10);
+        this.addObject(
+            new Robber(), 
+            Greenfoot.getRandomNumber(this.getWidth()), 
+            Greenfoot.getRandomNumber(this.getHeight())
+        );
         //this.addObject(new Police("8","2","4","6"), 15, 0);
         //this.addObject(new Police(), 5, 5);
     }
