03151a6c57b39324c64e3f48ecb7df5f9c3ff4d9
Josef Rak <peparak@gmail.com>
2024-07-03 09:30:25 +0200

    Add robber attributes and constructor

--- FromBeginning/Robber.java
+++ FromBeginning/Robber.java
@@ -8,12 +8,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Robber extends Actor
 {
+    private int delay;
+    private int delayCounter;
     /**
      * Act - do whatever the Robber wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Robber(){
+        this.delayCounter = 0;
+        this.delay = 3;
+    }
     public void act()
     {
         // Add your action code here.
+        
     }
 }
