7ba25788c9ae92e8238da3125fa38ba7d4789d8e
Josef Rak <peparak@gmail.com>
2024-07-03 10:29:48 +0200

    use inheritance in robber class

--- FromBeginning/Robber.java
+++ FromBeginning/Robber.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Robber extends Actor
+public class Robber extends Person
 {
     private int delay;
     private int delayCounter;
